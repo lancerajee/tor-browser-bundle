ee5e7274d8ae75b029024daf2adf920bb2f06840
David Fifield <david@bamsoftware.com>
2014-04-07 04:40:09 +0000

    Remove meek-client-with-extension.sh.

--- Bundle-Data/linux/Tor/PluggableTransports/meek-client-with-helper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./Browser/firefox -no-remote -profile Data/Browser/profile.meek-http-helper 2>&1 | while read line; do
-	if echo "$line" | grep -q '^meek-http-helper: listen 127.0.0.1:'; then
-		./Tor/PluggableTransports/meek-client "$@" --helper 127.0.0.1:"$(echo "$line" | sed -e 's/^meek-http-helper: listen 127\.0\.0\.1:\([0-9]\+\).*/\1/')"
-	fi
-done
